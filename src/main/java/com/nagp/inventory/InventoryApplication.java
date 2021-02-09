9e782c7e927f5bb87495020b2e8d8b9dc4113593
Nagp2018 <nitish.singla@nagarro.com>
2021-02-09 10:52:00 +0530

    update

--- src/main/java/com/nagp/inventory/InventoryApplication.java
+++ src/main/java/com/nagp/inventory/InventoryApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
